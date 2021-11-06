d67c33c055e6ec6cf7597183167d79fecd6f0a66
SudoShwetanshu <acharyashwetanshu@gmail.com>
2021-11-06 15:14:02 +0530

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+const cors = require('cors')
+
+app.use(cors())
+
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000;
 
